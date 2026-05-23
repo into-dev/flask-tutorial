274134461b3263d2b0378c67040c3bd91854d7ce
Andrey Shelyagin <andrey.sheliagin@gmail.com>
2026-05-23 20:31:44 +0500

    Add the detail view for post

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -99,3 +99,9 @@ def delete(id):
     db.execute('DELETE FROM post WHERE id = ?', (id, ))
     db.commit()
     return redirect(url_for('blog.index'))
+
+
+@bp.route('/<int:id>/detail', methods=('GET', ))
+def detail(id):
+    post = get_post(id, False)
+    return render_template('blog/detail.html', post=post)
